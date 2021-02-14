5277a143a1bd9d24e02e17aa5a828eff2b711f57
DustinThostenson <dustin@delta3consulting.com>
2021-02-14 15:03:56 -0600

    Update GildedRose.kt
    magicNumbers = constants - minimum days

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -7,9 +7,14 @@ class GildedRose(var items: Array<Item>) {
         val backstagePasses = "Backstage passes to a TAFKAL80ETC concert"
         val sulfuras = "Sulfuras, Hand of Ragnaros"
         val maxQuality = 50
+        val minimumSellInDays = 0
+        val minimumQuality = 0
+        val upcomingDays = 11
+        val thisWeek = 6
+
         items.forEach { item ->
             if (item.name != agedBrie && item.name != backstagePasses) {
-                if (item.quality > 0) {
+                if (item.quality > minimumQuality) {
                     if (item.name != sulfuras) {
                         item.quality = item.quality - 1
                     }
@@ -19,14 +24,12 @@ class GildedRose(var items: Array<Item>) {
                     item.quality = item.quality + 1
 
                     if (item.name == backstagePasses) {
-                        val upcomingDays = 11
                         if (item.sellIn < upcomingDays) {
                             if (item.quality < maxQuality) {
                                 item.quality = item.quality + 1
                             }
                         }
 
-                        val thisWeek = 6
                         if (item.sellIn < thisWeek) {
                             if (item.quality < maxQuality) {
                                 item.quality = item.quality + 1
@@ -40,10 +43,10 @@ class GildedRose(var items: Array<Item>) {
                 item.sellIn = item.sellIn - 1
             }
 
-            if (item.sellIn < 0) {
+            if (item.sellIn < minimumSellInDays) {
                 if (item.name != agedBrie) {
                     if (item.name != backstagePasses) {
-                        if (item.quality > 0) {
+                        if (item.quality > minimumQuality) {
                             if (item.name != sulfuras) {
                                 item.quality = item.quality - 1
                             }
