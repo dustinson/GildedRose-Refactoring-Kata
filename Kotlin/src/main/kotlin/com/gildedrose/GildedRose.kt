3b917e53c20524c843265760aca34d91b83a823e
DustinThostenson <dustin@delta3consulting.com>
2021-02-14 14:54:52 -0600

    Update GildedRose.kt
    superflous syntax: replace index with object

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -7,50 +7,51 @@ class GildedRose(var items: Array<Item>) {
         val backstagePasses = "Backstage passes to a TAFKAL80ETC concert"
         val sulfuras = "Sulfuras, Hand of Ragnaros"
         for (i in items.indices) {
-            if (items[i].name != agedBrie && items[i].name != backstagePasses) {
-                if (items[i].quality > 0) {
-                    if (items[i].name != sulfuras) {
-                        items[i].quality = items[i].quality - 1
+            val item = items[i]
+            if (item.name != agedBrie && item.name != backstagePasses) {
+                if (item.quality > 0) {
+                    if (item.name != sulfuras) {
+                        item.quality = item.quality - 1
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
 
-                    if (items[i].name == backstagePasses) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                    if (item.name == backstagePasses) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1
                             }
                         }
                     }
                 }
             }
 
-            if (items[i].name != sulfuras) {
-                items[i].sellIn = items[i].sellIn - 1
+            if (item.name != sulfuras) {
+                item.sellIn = item.sellIn - 1
             }
 
-            if (items[i].sellIn < 0) {
-                if (items[i].name != agedBrie) {
-                    if (items[i].name != backstagePasses) {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != sulfuras) {
-                                items[i].quality = items[i].quality - 1
+            if (item.sellIn < 0) {
+                if (item.name != agedBrie) {
+                    if (item.name != backstagePasses) {
+                        if (item.quality > 0) {
+                            if (item.name != sulfuras) {
+                                item.quality = item.quality - 1
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality
+                        item.quality = item.quality - item.quality
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
                     }
                 }
             }
