7127d1d519f5b0f7cea8e328ee7fcc4bef5a3bb6
DustinThostenson <dustin@delta3consulting.com>
2021-02-14 18:49:33 -0600

    Update GildedRose.kt
    reducing nesting, bringing backstage passes up

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -18,15 +18,14 @@ class GildedRose(var items: Array<Item>) {
 
             if (item.name == agedBrie) {
                 incrementQuality(item, maxQuality)
+            } else if (item.name == backstagePasses) {
+                updateBackstagePassQuality(item, upcomingDays, maxQuality, thisWeek)
             } else {
-                if (item.name == backstagePasses) {
-                    updateBackstagePassQuality(item, upcomingDays, maxQuality, thisWeek)
-                } else {
-                    if (item.quality > minimumQuality) {
-                        item.quality = item.quality - 1
-                    }
+                if (item.quality > minimumQuality) {
+                    item.quality = item.quality - 1
                 }
             }
+            
 
             item.sellIn = item.sellIn - 1
 
