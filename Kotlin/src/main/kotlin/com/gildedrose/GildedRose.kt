8d10f296aa1396dfbd65f275679a5a3d2498576d
DustinThostenson <dustin@delta3consulting.com>
2021-02-14 19:51:48 -0600

    Update GildedRose.kt
    moved logic for base product managment out.  code looks more busy now

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,13 +1,13 @@
 package com.gildedrose
 
-class GildedRose(var items: Array<Item>) {
+private const val minimumSellInDays = 0
+private const val maxQuality = 50
+private const val minimumQuality = 0
 
+class GildedRose(var items: Array<Item>) {
     private val agedBrie = "Aged Brie"
     private val backstagePasses = "Backstage passes to a TAFKAL80ETC concert"
     private val sulfuras = "Sulfuras, Hand of Ragnaros"
-    private val maxQuality = 50
-    private val minimumSellInDays = 0
-    private val minimumQuality = 0
     private val upcomingDays = 11
     private val thisWeek = 6
     fun updateQuality() {
@@ -17,46 +17,31 @@ class GildedRose(var items: Array<Item>) {
             if (item.name == sulfuras)
                 return
             else if (item.name == agedBrie) {
-                updateQuality(item)
+                standardProductManager.updateQuality(item)
                 standardProductManager.decrementSellIn(item)
-                if (isOldProperty(item)) updateQuality(item)
+                if (standardProductManager.isOldProperty(item)) updateQuality(item)
             } else if (item.name == backstagePasses) {
-                updateQuality(item)
+                standardProductManager.updateQuality(item)
                 if (item.sellIn < upcomingDays) {
-                    updateQuality(item)
+                    standardProductManager.updateQuality(item)
                 }
                 if (item.sellIn < thisWeek) {
-                    updateQuality(item)
+                    standardProductManager.updateQuality(item)
                 }
                 standardProductManager.decrementSellIn(item)
-                if (isOldProperty(item)) clearQuality(item)
+                if (standardProductManager.isOldProperty(item)) standardProductManager.clearQuality(item)
             } else {
-                decrementQuality(item)
+                standardProductManager.decrementQuality(item)
                 standardProductManager.decrementSellIn(item)
 
-                if (isOldProperty(item)) {
-                    decrementQuality(item)
+                if (standardProductManager.isOldProperty(item)) {
+                    standardProductManager.decrementQuality(item)
                 }
             }
         }
     }
 
-    private fun decrementQuality(item: Item) {
-        if (item.quality > minimumQuality) {
-            item.quality = item.quality - 1
-        }
-    }
-
-    private fun updateQuality(item: Item) {
-        if (item.quality < maxQuality) {
-            item.quality = item.quality + 1
-        }
-    }
-
-    private fun isOldProperty(item: Item) = item.sellIn < minimumSellInDays
-
-    private fun clearQuality(item: Item) {
-        item.quality = 0
+    private
     }
 
 
@@ -66,5 +51,22 @@ class StandardProductManager {
     fun decrementSellIn(item: Item) {
         item.sellIn = item.sellIn - 1
     }
+
+    fun updateQuality(item: Item) {
+        if (item.quality < maxQuality) {
+            item.quality = item.quality + 1
+        }
+    }
+    fun decrementQuality(item: Item) {
+        if (item.quality > minimumQuality) {
+            item.quality = item.quality - 1
+        }
+
+    }
+    fun clearQuality(item: Item) {
+        item.quality = 0
+    }
+
+    fun isOldProperty(item: Item) = item.sellIn < minimumSellInDays
 }
 
